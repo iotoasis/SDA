f4b5c588ca68bc585afcaf001ec46b24b46b4526
Lucia <Lucia@10.10.10.125>
2017-06-29 11:08:19 +0900

    cm POST를 위한 CmReqDTO 클래스 생성

---
+++ sda-web/src/main/java/com/pineone/icbms/sda/itf/cm/dto/CmReqDTO.java
@@ -0,0 +1,47 @@
+package com.pineone.icbms.sda.itf.cm.dto;
+
+public class CmReqDTO {
+	
+	private String cmid;
+	private String ciid;
+	private String cmname;
+	private String execution_type;
+	private String cm_remarks;
+	public String getCmid() {
+		return cmid;
+	}
+	public void setCmid(String cmid) {
+		this.cmid = cmid;
+	}
+	public String getCiid() {
+		return ciid;
+	}
+	public void setCiid(String ciid) {
+		this.ciid = ciid;
+	}
+	public String getCmname() {
+		return cmname;
+	}
+	public void setCmname(String cmname) {
+		this.cmname = cmname;
+	}
+	public String getExecution_type() {
+		return execution_type;
+	}
+	public void setExecution_type(String execution_type) {
+		this.execution_type = execution_type;
+	}
+	public String getCm_remarks() {
+		return cm_remarks;
+	}
+	public void setCm_remarks(String cm_remarks) {
+		this.cm_remarks = cm_remarks;
+	}
+	
+	@Override
+	public String toString() {
+		return "CmReqDTO [cmid=" + cmid + ", ciid=" + ciid + ", cmname=" + cmname + ", execution_type=" + execution_type
+				+ ", cm_remarks=" + cm_remarks + "]";
+	}
+
+}
